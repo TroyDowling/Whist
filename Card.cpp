c9a26bf176abf6d4776f0265c7943e338d202c42
Anders <olson16@stolaf.edu>
2017-11-17 10:14:31 -0600

    updated (but not completed) Card.h

---
+++ Card.cpp
@@ -0,0 +1,14 @@
+#include "Card.h"
+
+bool Card::mouse_over(int x, int y)
+{
+	if(x >= x_pos && x <= x_pos + w)
+	{
+		if(y >= y_pos && y <= y_pos + h)
+		{
+			return true;
+		}
+		else return false;
+	}
+	else return false;
+}
--- Card.h
+++ Card.h
@@ -4,23 +4,31 @@
 class Card
 {
 	protected:
-	enum cardvals {
-		H_A, H_2, H_3, H_4, H_5, H_6, H_7, H_8, H_9, H_10, H_J, H_Q, H_K,
-		C_A, C_2, C_3, C_4, C_5, C_6, C_7, C_8, C_9, C_10, C_J, C_Q, C_K, 
-		S_A, S_2, S_3, S_4, S_5, S_6, S_7, S_8, S_9, S_10, S_J, S_Q, S_K,
-		D_A, D_2, D_3, D_4, D_5, D_6, D_7, D_8, D_9, D_10, D_J, D_Q, D_K 
-	} val;
+	int x_pos, y_pos w, h;
+	
 	public:
+	//2=0 - Ace = 12
+	enum cardvals {
+		two, three, four, five, six, seven,
+		eight, nine, ten, jack, queen, king, ace
+	};
+	//clubs, hearts, spades, diamonds
+	enum suit {c, h, s, d};
+	
 	//Enum implementaion is a little broken, but we don't need it for the demo.
-	//I'll comment it out for now -Bennett
-	Card(); //Temp default constr.
-	//Card(int v){if(v >= 0 && v < 52) val = v;}
-	bool Clicked(int x,int y); //Not finished
+	//I'll comment it out for now -Bennett (Should work now) -AO
+	
+	Card(){val = -1; }
+	
+	//Constructor
+	Card(int v){if(v >= 0 && v < 52) val = v;}
+	
+	bool mouse_over(int x,int y);
 	//void show(); <- Good chance this won't be used
 	//void animate(); <- Same here, both of these ought to be handled elsewhere
+	
 	int get_val () {return val;}
-	//changed int to void return, removed temp.
-	//void set_val (int v) {val = v;}
+	void set_val (int v) {val = v;}
 };
 
 #endif //_CARD_H_
