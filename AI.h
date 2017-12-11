51c81ffb4cc74017284a24147356bdb25ab6393c
Anders <olson16@stolaf.edu>
2017-12-11 09:50:42 -0600

    fix

--- AI.h
+++ AI.h
@@ -1,1 +1,26 @@
-//New file
+#ifndef _AI_H_
+#define _AI_H_
+#include "Gamestate.h"
+#include "Card.h"
+#include "hand.h"
+
+class AI
+{
+	int len, id, partner;
+	bool invited = false;
+
+	/* There will be different algorithms in
+	 * makePlay that correspond to difficulty
+	 * settings. 0 = easy, 1 = interm, etc.
+	 */
+	short difficulty;
+
+	public:
+	//gamestate reference, hand ID, diff
+
+	AI(const Gamestate&, int, short);
+	
+	Card * makePlay(Gamestate&);
+};
+
+#endif //_AI_H_
